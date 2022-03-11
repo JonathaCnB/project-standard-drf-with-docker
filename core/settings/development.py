e5b1708f42d078eaa1ef683f48dcb7c913e032f8
Jonatha Carlos <jotacarlosftb@gmail.com>
2022-03-11 00:51:39 -0400

    chore: corrigido proglema de localização de importação

--- core/settings/development.py
+++ core/settings/development.py
@@ -1,4 +1,22 @@
-from .base import *
+from pathlib import Path
+
+import environ
+
+from .base import *  # noqa
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(BASE_DIR / '.env')
+
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("SENDGRID_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("SENDGRID_PORT")
+EMAIL_HOST_USER = env("SENDGRID_USER")
+EMAIL_HOST_PASSWORD = env("HOST_MAIL_KEY")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM")
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Project Standard"
 
 DATABASES = {
     "default": {
